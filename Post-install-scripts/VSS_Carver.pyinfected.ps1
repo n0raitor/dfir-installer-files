23779c65fabb8563827b0e86feed3160d562b567
N. Schmidt <n0raitor@pm.me>
2026-03-12 09:42:54 +0100

    VSS_Carver fix

--- Post-install-scripts/VSS_Carver.pyinfected.ps1
+++ Post-install-scripts/VSS_Carver.pyinfected.ps1
@@ -1,4 +1,4 @@
-# PowerShell PostProcess script for VSS_Carver.pyinfected of Category C:\DFIR\_Tools\Carving
+# PowerShell PostProcess script for VSS_Carver of Category C:\DFIR\_Tools\Carving
 
 # Check if the category path exists, if not, create it
 if (!(Test-Path -Path "C:\DFIR\_Tools\Carving")) {
@@ -6,15 +6,15 @@ if (!(Test-Path -Path "C:\DFIR\_Tools\Carving")) {
 }
 
 # Check if the tool directory exists
-if (Test-Path -Path "C:\DFIR\VSS_Carver.pyinfected") {
+if (Test-Path -Path "C:\DFIR\VSS_Carver") {
     # Create symlink if tool directory exists
-    New-Item -Path "C:\DFIR\_Tools\Carving\VSS_Carver.pyinfected" -ItemType SymbolicLink -Target "C:\DFIR\VSS_Carver.pyinfected" -Force
+    New-Item -Path "C:\DFIR\_Tools\Carving\VSS_Carver" -ItemType SymbolicLink -Target "C:\DFIR\VSS_Carver" -Force
 } else {
-    Write-Host "Tool directory does not exist: C:\DFIR\VSS_Carver.pyinfected"
+    Write-Host "Tool directory does not exist: C:\DFIR\VSS_Carver"
 }
 
 # Create symlink
-#New-Item -Path "C:\DFIR\_Tools\Carving\VSS_Carver.pyinfected" -ItemType SymbolicLink -Target "C:\DFIR\VSS_Carver.pyinfected\VSS_Carver.pyinfected.exe" -Force
+#New-Item -Path "C:\DFIR\_Tools\Carving\VSS_Carver" -ItemType SymbolicLink -Target "C:\DFIR\VSS_Carver\VSS_Carver.exe" -Force
 
 # File moving command will be inserted here
 # Move-Item -Path C:\DFIR\ -Destination "C:\DFIR\Tools\Carving"
